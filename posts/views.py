67e8fb192a507f475fda3437aa736affb97c3c59
ScorpiOVN <scorpiovn.team@gmail.com>
2026-05-13 17:08:32 +0400

    Добавлен views.py

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from .models import Post, Comment
+from .forms import CommentForm
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'posts/post_list.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'posts/post_detail.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = self.object.comments.all()
+        context['form'] = CommentForm()
+        return context
+
+def add_comment(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    return redirect('post_detail', pk=post.pk)
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'posts/post_create.html'
+    fields = ['title', 'content']
+    success_url = reverse_lazy('post_list')
