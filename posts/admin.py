4a1c2ea5471c984eb65b9a1787105d82fd2c5f9e
ScorpiOVN <scorpiovn.team@gmail.com>
2026-05-13 17:04:57 +0400

    Модели добавлены в админ-панель

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at', 'updated_at']
+    list_filter = ['created_at']
+    search_fields = ['title', 'content']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author', 'post', 'text', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['author', 'text']
